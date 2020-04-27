f565ccfafba698a4b511127bc60f070ffe704955
Ed Posey <eposey89@gmail.com>
2020-04-26 22:55:38 -0400

    fixes markup

---
+++ sketch.docset/Contents/Resources/Documents/assets/shortcuts-e8f8c1eb662f6f4e71d289a489fedbbd3a1961666df11aa4fc6bab02f70653e1.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+		
+	// FANCYBOX
+	$(".signup").fancybox({
+		maxWidth: 450,
+		minHeight: 505,
+		autoHeight: true,
+		autoSize: true,
+		fitToView: true,
+		padding: 0,
+		
+		openSpeed: 250,
+		closeSpeed: 100,
+		
+		openEffect: 'elastic',
+		closeEffect: 'elastic'
+	});
+});
